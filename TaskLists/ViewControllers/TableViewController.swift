3329dd9ad847cf4d14e8dcf1435704ad4b67ef3f
AtesiT <atesit@mail.ru>
2026-01-08 15:30:19 +0300

    MADE: added showAlert, and replaced the mock daatas on real array in Model

--- TaskLists/ViewControllers/TableViewController.swift
+++ TaskLists/ViewControllers/TableViewController.swift
@@ -2,26 +2,51 @@ import UIKit
 
 final class TableViewController: UITableViewController {
     
-    let countLinesData = ["1","2"]
-
+    private var taskLists: [TaskList]!
+    private let storageManager = StorageManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let addButton = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonPressed)
+        )
+        
+        navigationItem.rightBarButtonItem = addButton
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        taskLists = storageManager.fetchTrackList()
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        countLinesData.count
+        taskLists.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let theData = countLinesData[indexPath.row]
+        let theData = taskLists[indexPath.row]
         var content = cell.defaultContentConfiguration()
         
-        content.text = theData
+        content.text = theData.title
+        content.secondaryText = theData.tasks.count.formatted()
         
         cell.contentConfiguration = content
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let taskList = taskLists
     }
 }
