e01e94f55a3615df954da9a041cc609402e27ac5
AtesiT <atesit@mail.ru>
2026-01-08 13:46:47 +0300

    MADE: Model of tasks, view of table in storyboard

---
+++ TaskLists/Models/TaskList.swift
@@ -0,0 +1,15 @@
+import Foundation
+import RealmSwift
+
+final class TaskList {
+    var title = ""
+    var date = Date()
+    var tasks: [Task] = []
+}
+
+final class Task {
+    var title = ""
+    var note = ""
+    var date = Date()
+    var isComplete = false
+}
