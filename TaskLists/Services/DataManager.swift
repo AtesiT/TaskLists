e104af1625c4f435fef6e4d84ac00fe190ae4930
AtesiT <atesit@mail.ru>
2026-01-10 17:28:07 +0300

    ADD: made init by (value: Any) and add by insert([])

--- TaskLists/Services/DataManager.swift
+++ TaskLists/Services/DataManager.swift
@@ -16,6 +16,13 @@ final class DataManager {
         milk.title = "Milk"
         milk.note = "2L"
         
+        let apples = Task(value: ["Apples", "2Kg"])
+        //  let bread = Task(value: ["Bread", "", Date(), true])
+        //  Используем ключи, чтобы задать только нужные значения
+        let bread = Task(value: ["title": "Bread", "isComplete": true])
+        
         shoppingList.tasks.append(milk)
+        //  Если индекс поставить 0, то milk будет третьим, т.к. присваивается на место указанного индекса
+        shoppingList.tasks.insert(contentsOf: [apples, bread], at: 1)
     }
 }
