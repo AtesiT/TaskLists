ba363a200ab7c37b25e55b91de39e8ab2748c7ea
AtesiT <atesit@mail.ru>
2026-01-10 17:12:38 +0300

    ADD: first task was added in our temporary data

--- TaskLists/Services/DataManager.swift
+++ TaskLists/Services/DataManager.swift
@@ -1,11 +1,21 @@
 import Foundation
+import RealmSwift
 
 final class DataManager {
     static let shared = DataManager()
     
     private init() {}
     
+    //  Здесь добавляем параметр completion для того, чтобы мы смогли раскрыть блок замыкания и внутри него обновить интерфейс
     func createTempData(completion: @escaping () -> Void) {
+        let shoppingList = TaskList()
+        shoppingList.title = "Shopping List"
         
+        //  Создаем пустой экземпляр класса со значениями по умолчанию. После создания - обновляем значения свойств.
+        let milk = Task()
+        milk.title = "Milk"
+        milk.note = "2L"
+        
+        shoppingList.tasks.append(milk)
     }
 }
