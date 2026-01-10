d487ab7dafd2cb5f2e8bc3c76658dc0f1cfd4277
AtesiT <atesit@mail.ru>
2026-01-10 14:48:56 +0300

    MADE: fill up sections and rows of our table

--- TaskLists/ViewControllers/TasksTableViewController.swift
+++ TaskLists/ViewControllers/TasksTableViewController.swift
@@ -16,16 +16,46 @@ final class TasksTableViewController: UITableViewController {
             target: self,
             action: #selector(addButtonPressed)
         )
-        navigationItem.rightBarButtonItem = [addButton, editButtonItem]
+        navigationItem.rightBarButtonItems = [addButton, editButtonItem]
     }
 
     // MARK: - Table view data source
+    
+    
+    //  Для того, чтобы отобразить только две секции Current Tasks and Completed Tasks
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
 
+    //  Задаём количество ячеек в одной из секций
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        section == 0 ? currentTasks.count : completedTasks.count
+    }
+
+    //  Задаём заголовок каждой ячейке
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "CURRENT TASKS" : "COMPLETED TASKS"
     }
     
+    //  Заполняем ячейки содержимым
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        
+        content.text = task.title
+        content.secondaryText = task.note
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    @objc private func addButtonPressed() {
+        
     }
 }
+
+//#Preview {
+//    TasksTableViewController()
+//}
